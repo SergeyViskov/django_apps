31a3e20cf3eebd9e190635e8948d12cf59e5e3ab
Пользователь <cocosi383@gmail.com>
2023-03-15 22:38:34 +0300

    настроил категории вопросов

--- quizapp/main/urls.py
+++ quizapp/main/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/register', views.register, name='register'),
     path('all-categories', views.all_categories, name='all_categories'),
+    path('category-questions/<int:cat_id>', views.category_questions, name='category_questions'),
+    path('submit-answer/<int:cat_id>/<int:quest_id>', views.submit_answer, name='submit_answer'),
 ]
--- quizapp/main/views.py
+++ quizapp/main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 from . import forms
 from . import models
@@ -29,3 +31,37 @@ def all_categories(request):
         'data': cat_data
     }
     return render(request, 'all-category.html', context)
+
+
+@login_required
+def category_questions(request, cat_id):
+    category = models.QuizCategory.objects.get(id=cat_id)
+    question = models.QuizQuestions.objects.filter(
+        category=category).order_by('id').first()
+    context = {
+        'question': question,
+        'category': category
+    }
+    return render(request, 'category-questions.html', context)
+
+
+@login_required
+def submit_answer(request, cat_id, quest_id):
+    if request.method == 'POST':
+        category = models.QuizCategory.objects.get(id=cat_id)
+        question = models.QuizQuestions.objects.filter(
+            category=category, id__gt=quest_id).exclude(
+                id=quest_id).order_by('id').first()
+        context = {
+            'question': question,
+            'category': category
+        }
+        if 'skip' in request.POST:
+            if question:
+                return render(request, 'category-questions.html', context)
+        if question:
+            return render(request, 'category-questions.html', context)
+        else:
+            return HttpResponse('No more questions!')
+    else:
+        return HttpResponse('Method not allowed!')
